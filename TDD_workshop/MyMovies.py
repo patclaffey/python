4d3c32b22716702544b322af84c6fbc9e96dd84b
Pat Claffey <patclaffey@gmail.com>
2015-03-09 07:18:59 +0000

    unittest workshop exercise- Movies exercise

--- TDD_workshop/MyMovies.py
+++ TDD_workshop/MyMovies.py
@@ -1,3 +1,31 @@
+'''
+Given the following input of movie titles and years released
+
+Jaws (1975)
+Starwars 1977
+2001 A Space Odyssey ( 1968 )
+Back to the future 1985.
+Raiders of the lost ark 1981 .
+jurassic park 1993
+The Matrix 1999
+A fist full of Dollars
+10,000 BC (2008)
+1941 (1979)
+24 Hour Party People (2002)
+300 (2007)
+2010
+
+
+Produce.
+
+2000s : 3
+1970s : 3
+1980s : 2
+1990s : 2
+1960s : 1
+
+'''
+
 import re
 
 class Movies(object):
